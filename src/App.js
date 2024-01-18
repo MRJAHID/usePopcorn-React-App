1b5eafe16c211a7cb4a25d525a6577ff502aa682
MRJAHID <mrjahid350@gmail.com>
2024-01-19 01:16:52 +0600

    Condition Error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from "react";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
-import {tempMovieData, tempWatchedData} from "./data";
 import Search from "./components/Search";
 import NumResults from "./components/NumResults";
 import MovieList from "./components/MovieList";
@@ -9,6 +8,7 @@ import Box from "./components/Box";
 import WatchedSummary from "./components/WatchedSummary";
 import WatchedMovieList from "./components/WatchedMovieList";
 import Loader from "./components/Loader";
+import ErrorMessage from "./components/ErrorMessage";
 
 const KEY = 'c873b0f1';
 
@@ -16,14 +16,27 @@ export default function App() {
     const [movies, setMovies] = useState([]);
     const [watched, setWatched] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState('');
 
     useEffect(function () {
         async function fetchMovies() {
-            setIsLoading(true);
-            const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`);
-            const data = await res.json();
-            setMovies(data.Search);
-            setIsLoading(false);
+            try {
+                setIsLoading(true);
+                const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstjsjsjsjkellar`);
+
+                if (!res.ok) {
+                    throw new Error('Something went wrong with fetching the movies')
+                }
+
+                const data = await res.json();
+                if (data.Response === 'False') throw new Error('Movie Not Found')
+
+                setMovies(data.Search);
+            } catch (e) {
+                setError(e.message);
+            } finally {
+                setIsLoading(false);
+            }
         }
 
         fetchMovies();
@@ -37,7 +50,10 @@ export default function App() {
             </Navbar>
             <Main>
                 <Box>
-                    {isLoading ? <Loader/> : <MovieList movies={movies}/>}
+                    {/*{isLoading ? <Loader/> : <MovieList movies={movies}/>}*/}
+                    {isLoading && <Loader/>}
+                    {!isLoading && !error && <MovieList movies={movies}/>}
+                    {error && <ErrorMessage message={error}/>}
                 </Box>
                 <Box>
                     <WatchedSummary watched={watched}/>
